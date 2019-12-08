bf6cc5c8021c1e96e32558e936695158d8e8b4bb
Tom Robertshaw <me@tomrobertshaw.net>
2019-12-08 19:11:37 +0000

    Stream blog posts out to json.

--- src/Commands/ExportCommand.php
+++ src/Commands/ExportCommand.php
@@ -5,6 +5,8 @@ namespace Space48\HubSpotBlogExport\Commands;
 use Space48\HubSpotBlogExport\BlogPostService;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
+use Symfony\Component\Console\Style\SymfonyStyle;
+use Violet\StreamingJsonEncoder\StreamJsonEncoder;
 
 class ExportCommand extends BaseCommand
 {
@@ -23,7 +25,7 @@ class ExportCommand extends BaseCommand
     protected function configure()
     {
         parent::configure();
-        $this->setDescription('Exports all blog posts from HubSpot');
+        $this->setDescription('Exports all blog posts from HubSpot as json');
     }
 
     /**
@@ -34,20 +36,23 @@ class ExportCommand extends BaseCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $output->writeln("Starting blog export");
-
         $blogPostService = new BlogPostService($this->getApiKey($input));
 
         try {
             $pageGenerator = $blogPostService->getPage();
         } catch (\Exception $e) {
-            $output->writeln("<error>" . $e->getMessage() . "</error>");
+            $io = new SymfonyStyle($input, $output);
+            $io->getErrorStyle()->error("<error>" . $e->getMessage() . "</error>");
             return 1;
         }
 
-        foreach ($pageGenerator as $page) {
-            var_dump($page);
-        }
+        $jsonEncoder = new StreamJsonEncoder(
+            $pageGenerator,
+            function($json) use ($output) {
+                $output->write($json);
+            }
+        );
+        $jsonEncoder->encode();
 
         return 0;
     }
